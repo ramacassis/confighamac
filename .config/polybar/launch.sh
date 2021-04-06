0d28299098a81077938d9ebdc3f4bf861b9e0ad6
Rayan <rayan.mac@outlook.fr>
2021-04-06 02:05:24 +0200

    polybar: set up minimal polybar
    - nice pal config: hbuyse/dotfiles

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+POLYBAR="$(command -v polybar)"
+XRANDR="$(command -v xrandr)"
+
+# Check that the commands exists
+if [[ -z "$POLYBAR" ]]; then
+    exit 1
+fi
+
+# Terminate already running bar instances
+killall -q "$POLYBAR"
+
+# If all your bars have ipc enabled, you can also use polybar-msg cmd quit
+if [[ -n "$XRANDR" ]]; then
+    for m in $($XRANDR --query | grep -w "connected" | cut -d" " -f1); do
+        MONITOR="$m" $POLYBAR --reload top 2> "/tmp/polybar_${m}_top.log" &
+        MONITOR="$m" $POLYBAR --reload bottom 2> "/tmp/polybar_${m}_bottom.log" &
+    done
+else
+    $POLYBAR --reload top 2> /tmp/polybar_top.log &
+    $POLYBAR --reload bottom 2> /tmp/polybar_bottom.log &
+fi
