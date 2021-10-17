2758c5e4b6d71be43d6fe3e984010e378a91f6ab
Rayan <rayan.mac@outlook.fr>
2021-10-17 23:48:09 +0200

    polybar: split config in multiple files

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -14,10 +14,10 @@ killall -q "$POLYBAR"
 # If all your bars have ipc enabled, you can also use polybar-msg cmd quit
 if [[ -n "$XRANDR" ]]; then
     for m in $($XRANDR --query | grep -w "connected" | cut -d" " -f1); do
-        MONITOR="$m" $POLYBAR --reload top 2> "/tmp/polybar_${m}_top.log" &
+        #MONITOR="$m" $POLYBAR --reload top 2> "/tmp/polybar_${m}_top.log" &
         MONITOR="$m" $POLYBAR --reload bottom 2> "/tmp/polybar_${m}_bottom.log" &
     done
 else
-    $POLYBAR --reload top 2> /tmp/polybar_top.log &
+    #$POLYBAR --reload top 2> /tmp/polybar_top.log &
     $POLYBAR --reload bottom 2> /tmp/polybar_bottom.log &
 fi
